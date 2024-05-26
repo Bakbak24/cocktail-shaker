a15acfc124ec47cf6f729d8e5e0976699d6d28ab
Bakbak24 <zakabak3@gmail.com>
2024-05-26 23:13:13 +0200

    Added shake points and a timer

--- js/main.js
+++ js/main.js
@@ -1,9 +1,58 @@
 window.addEventListener('deviceorientation', handleOrientation);
 
+let shakeCount = 0;
+let countdown = 30;
+let shaking = false;
+let lastShakeTime = 0;
+let startAgainButton = document.getElementById('start-btn');
+
 function handleOrientation(event) {
     let beta = event.beta; 
     let rotation = beta - 90;
     
     let shaker = document.getElementById('shaker');
     shaker.style.transform = "rotate("+rotation+"deg)";
+    
+    let currentTime = new Date().getTime();
+    if (Math.abs(rotation) > 30) {
+        if (!shaking || currentTime - lastShakeTime > 1000) {
+            shaking = true;
+            lastShakeTime = currentTime;
+            shakeCount++;
+            document.getElementById('shakes').innerText = "Shakes:" + shakeCount;
+        }
+    } else {
+        shaking = false;
+    }
 }
+
+function startGame() {
+    let countdownElement = document.getElementById('countdown');
+    let countdownInterval = setInterval(function() {
+        countdown--;
+        countdownElement.innerText = "Sec:" + countdown;
+        if (countdown <= 0) {
+            clearInterval(countdownInterval);
+            startAgainButton.style.display = "block";
+            endGame();
+        }
+    }, 1000);
+}
+
+function endGame() {
+    document.getElementById('title').innerText = "Time's up! You made " + shakeCount + " shakes.";
+    shakeCount = 0;
+    countdown = 30;
+    document.getElementById('shakes').innerText = "Shakes:0";
+    document.getElementById('countdown').innerText = "Sec:30";
+}
+
+startAgainButton.onclick = function() {
+    startGame();
+    startAgainButton.style.display = "none";
+    document.getElementById('title').innerText = "Keep shaking your cocktail as fast as possible for 30sec But not too fast or it will explode";
+};
+
+window.onload = function() {
+    startGame();
+};
